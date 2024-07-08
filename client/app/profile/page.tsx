9eec69661bb814335725fd925d2adff51022bf0c
icep0ps <tapsmuko@gmail.com>
2024-07-08 15:39:17 +0200

    feat: added profile page

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,33 +1,47 @@
+import axios from 'axios';
 import React from 'react';
 import { NextPage } from 'next';
 
-import Post from '@/components/explore/Post';
-import { Article } from '@/libs/types/types';
-
-const getProfile = async () => {};
+import { IArticle } from '../../../types';
+import { Button } from '@/components/ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import ExploreArticle from '@/components/ui/article/exploreArticle';
 
 type Props = {};
 
-const Profile: NextPage<Props> = (props) => {
-  const posts: Article[] = [];
+async function getPosts() {
+  return axios.get<IArticle[]>('http://localhost:3001/latest').then((data) => data.data);
+}
+
+const Profile: NextPage<Props> = async (props) => {
+  const posts = await getPosts();
 
   return (
     <div className="h-full">
-      <h1 className="text-2xl mb-5">Your Profile</h1>
+      <h1 className="text-2xl mb-5">Profile</h1>
 
       <div className="flex items-center gap-5">
-        <div className="h-20 w-20 rounded-full bg-sky-600" />
-        <div>
-          <h3>Username</h3>
-          <p>username@gmail.com</p>
+        <Avatar className="w-20 h-20">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col gap-2">
+          <div>
+            <h3>Tapiwa</h3>
+            <p className="text-sm text-muted-foreground">tapiwa@gmail.com</p>
+          </div>
+
+          <Button className="rounded-full">Edit Profile</Button>
         </div>
       </div>
 
-      <h3 className="text-xl my-5">Saved articles</h3>
-      <div id="saves">
-        {posts?.map((post) => (
-          <Post key={post.title} post={post} />
-        ))}
+      <h3 className="text-base mb-2  mt-5 font-bold">Favorites</h3>
+      <div className="flex flex-col gap-4">
+        {posts ? (
+          posts.map((post) => <ExploreArticle key={post.title} article={post} />)
+        ) : (
+          <p>No Saved articles yet</p>
+        )}
       </div>
     </div>
   );
