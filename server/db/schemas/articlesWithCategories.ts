f96ea52b02f90b4972d963e20f069a50ede03cb6
icep0ps <tapsmuko@gmail.com>
2024-07-11 16:54:59 +0200

    refactor: add pk

--- server/db/schemas/articlesWithCategories.ts
+++ server/db/schemas/articlesWithCategories.ts
@@ -1,13 +1,17 @@
 import { articles } from './articles';
 import { categories } from './categories';
 
-import { integer, pgTable, text } from 'drizzle-orm/pg-core';
+import { integer, pgTable, primaryKey, text } from 'drizzle-orm/pg-core';
 
-export const articlesWithCategories = pgTable('articlesWithCategories', {
-  articleId: integer('article_id')
-    .notNull()
-    .references(() => articles.id),
-  categoryId: text('category_id')
-    .notNull()
-    .references(() => categories.name),
-});
+export const articlesWithCategories = pgTable(
+  'articlesWithCategories',
+  {
+    articleId: integer('article_id')
+      .notNull()
+      .references(() => articles.id),
+    categoryId: text('category_id')
+      .notNull()
+      .references(() => categories.name),
+  },
+  (table) => ({ pk: primaryKey({ columns: [table.articleId, table.categoryId] }) })
+);
