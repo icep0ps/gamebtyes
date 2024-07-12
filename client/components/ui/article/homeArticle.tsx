226e9c16b4bbb03f89b949d0683b4bc8938d785e
icep0ps <tapsmuko@gmail.com>
2024-07-12 15:35:04 +0200

    feat: add functionality to read article button

--- client/components/ui/article/homeArticle.tsx
+++ client/components/ui/article/homeArticle.tsx
@@ -1,21 +1,17 @@
+'use client';
+
+import Link from 'next/link';
 import * as React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import { User, Gamepad } from 'lucide-react';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
-import Article, { useArticleContext } from './article';
 import { Button } from '../button';
 import { IArticle } from '../../../../types';
-import { User, Gamepad } from 'lucide-react';
+import Article, { useArticleContext } from './article';
 
 type IHomeArticleProps = { article: IArticle };
 
 function HomeArticleCover() {
-  const { author } = useArticleContext();
+  const { id, author } = useArticleContext();
 
   const metadata = [
     { title: author, icon: <User size={15} /> },
@@ -27,9 +23,12 @@ function HomeArticleCover() {
       withShadow
       className="h-full flex justify-end flex-col p-3  gap-4 pb-11 relative"
     >
-      <Button className="rounded-full text-sm self-start top-5 right-5 absolute">
-        Read article
-      </Button>
+      <Link href={'/posts/' + id}>
+        <Button className="rounded-full text-sm self-start top-5 right-5 absolute">
+          Read article
+        </Button>
+      </Link>
+
       <Article.title className="text-xl font-bold line-clamp-3" />
       <div className="flex gap-3">
         {metadata.map((data) => (
@@ -49,22 +48,11 @@ export default function HomeArticle(props: IHomeArticleProps) {
       article={props.article}
       className="min-h-[420px] rounded-3xl flex flex-col justify-between w-full gap-3 relative"
     >
-      <Swiper
-        slidesPerView={1}
-        scrollbar={{ draggable: true }}
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        className="h-full w-full flex flex-col relative"
-      >
-        <SwiperSlide className="h-full w-full ">
-          <HomeArticleCover />
-        </SwiperSlide>
-        <Article.content />
-      </Swiper>
-
+      <HomeArticleCover />
       {/* please make this more reusable */}
-      <div className="flex gap-3 absolute bottom-[-0.80rem] z-20">
-        <Button className="rounded-full text-sm">Save</Button>
-        <Button className="rounded-full text-sm">Share</Button>
+      <div className="flex gap-3 absolute bottom-[-0.80rem] ">
+        <Article.SaveBtn className="rounded-lg text-sm" />
+        <Button className="rounded-lg text-sm">Share</Button>
       </div>
     </Article>
   );
