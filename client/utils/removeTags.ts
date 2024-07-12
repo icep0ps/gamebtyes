79c190d284536784196f396c2c32dbd650f5eb4d
icep0ps <tapsmuko@gmail.com>
2024-07-12 15:33:17 +0200

    chore: remove unused utils

--- client/utils/removeTags.ts
+++
@@ -1,7 +0,0 @@
-const removeTags = (text: string) => {
-  if (text === null || text === '') return false;
-  else text = text.toString();
-  return text.replace(/(<([^>]+)>)/gi, '');
-};
-
-export default removeTags;
