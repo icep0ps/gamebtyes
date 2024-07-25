77d2911fff23f0239afb5ab2ae6494d1818e5682
icep0ps <tapsmuko@gmail.com>
2024-07-25 12:25:31 +0200

    style: improved layout

--- client/app/(pages)/explore/page.tsx
+++ client/app/(pages)/explore/page.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Article } from '@/libs/types/types';
-import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary } from "react-error-boundary";
 
-import ErrorComponent from '@/components/common/Error';
-import Filter from '@/components/common/Filter/Filter';
-import ExploreArticle from '@/components/ui/article/exploreArticle';
-import TrendingArticle from '@/components/ui/article/trendingArticle';
+import { type IArticle } from "../../../../types";
+import ErrorComponent from "@/components/common/Error";
+import Filter from "@/components/common/Filter/Filter";
+import ExploreArticle from "@/components/ui/article/exploreArticle";
+import TrendingArticle from "@/components/ui/article/trendingArticle";
 
 const getPosts = async () => {
-  const res = await fetch('http://localhost:3001/latest');
+  const res = await fetch("http://localhost:3001/latest");
   return res.json();
 };
 
 const Explore = () => {
-  const [posts, setPosts] = useState<Article[]>([]);
+  const [posts, setPosts] = useState<IArticle[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -27,7 +27,7 @@ const Explore = () => {
         setIsLoading(false);
       } catch (error) {
         setIsLoading(false);
-        throw new Error('Failed to load post: ' + error);
+        throw new Error("Failed to load post: " + error);
       }
     })();
   }, []);
@@ -37,26 +37,31 @@ const Explore = () => {
   return (
     <>
       <div className="flex flex-col gap-2">
-        <input type="text" value="search" className="bg-zinc-800 p-2 w-full rounded-xl" />
+        <input
+          type="text"
+          value="search"
+          className="bg-zinc-800 p-2 w-full rounded-xl"
+        />
       </div>
       <div className="flex gap-2 items-center">
         <Filter name="PC Gaming" />
         <Filter name="Mobile Gaming" />
         <Filter name="Console Gaming" />
       </div>
+      <div className="overflow-y-scroll max-h-screen">
+        <div className="flex flex-col gap-5 ">
+          <h1 className="font-bold text-xl">Trending</h1>
+          <TrendingArticle article={posts[8]} />
+        </div>
 
-      <div className="flex flex-col gap-5 ">
-        <h1 className="font-bold text-xl">Trending</h1>
-        <TrendingArticle article={posts[8]} />
-      </div>
-
-      <div className="flex flex-col gap-5 ">
-        <h1 className="font-bold text-xl">Popular</h1>
-        <ErrorBoundary FallbackComponent={ErrorComponent}>
-          {posts?.map((post) => (
-            <ExploreArticle key={post.title} article={post} />
-          ))}
-        </ErrorBoundary>
+        <div className="flex flex-col gap-5 ">
+          <h1 className="font-bold text-xl">Popular</h1>
+          <ErrorBoundary FallbackComponent={ErrorComponent}>
+            {posts?.map((post) => (
+              <ExploreArticle key={post.title} article={post} />
+            ))}
+          </ErrorBoundary>
+        </div>
       </div>
     </>
   );
