7bbc0810cf76c3b06ef3b1b987bbdc822928a924
icep0ps <tapsmuko@gmail.com>
2024-07-25 12:34:51 +0200

    feat: added lastest controller

--- server/api/controllers/latest.ts
+++ server/api/controllers/latest.ts
@@ -1,9 +1,15 @@
 import { type Request, type Response } from 'express';
+
 import db from '../../db/client';
 import { articles } from '../../db/schemas/articles';
 
 export default {
   get: async function (request: Request, response: Response) {
-    return await db.select().from(articles);
+    try {
+      const data = await db.select().from(articles);
+      return response.json(data);
+    } catch (error) {
+      // add error handling
+    }
   },
 };
